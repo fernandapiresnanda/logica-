b511e57ebc85e208340f7b48522cfe31e64a2b02
fernandapiresnanda <50593933+fernandapiresnanda@users.noreply.github.com>
2019-05-13 15:28:51 -0300

    Dados: primeira versão

---
+++ 1info1 - logica/logicadados.py
@@ -0,0 +1,13 @@
+from random import randint
+
+print("Jogo de Dados") 
+
+dados1 = randint(1, 6)
+dados2 = randint(1, 6)
+dados3 = randint(1, 6)
+dados4 = randint(1, 6)
+
+print("dado 1:", dados1)
+print("dado 2:", dados2)
+print("dado 3:", dados3)
+print("dado 4:", dados4)
